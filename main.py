55d42542569b57e2e90c6a2304e86677e6331613
hiatus770-chromebook <hiatus.cool@gmail.com>
2022-03-09 12:07:17 -0500

    mogus

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
+import os
 
+# Directories #
+txt = "input.txt"
+py = "compiled.py"
+
+# Functions # 
+def readTxt():
+    with open(txt, "r") as ipt:
+        lines = ipt.readlines() 
+        return lines
+
+def printFile(dir = py):
+    with open(dir, "r") as ipt:
+        lines = ipt.readlines() 
+        for line in lines:
+            print(line, end = "")
+
+def compile():
+    ipt = readTxt()
+    write = open(py, "w")
+    write.writelines(ipt)
+         
+
+def runCode():
+    "nothing" 
+
+readTxt()
+compile() 
+printFile()

a5c2a4d9e3388ba5054ea135018d76a6d1562e9c
hiatus770-chromebook <hiatus.cool@gmail.com>
2022-03-10 11:46:56 -0500

    among us

--- compiled.py
+++ compiled.py
@@ -1,4 +1,1 @@
-
-
-for i in range(10):
-    print(i)
+print("Among us")
--- main.py
+++ main.py
@@ -18,16 +18,56 @@ def printFile(dir = py):
             print(line, end = "")
     print()
 
+def splitAlgo(l):
+    methods = {}
+
+    for cnt in range(len(l)):
+        i = l[cnt]
+        if i == '"':
+            quoteFind = cnt
+            while(true):
+                if (l[quoteFind] == '"'):
+                    break;  
+                else: 
+                    quoteFind = quoteFind + 1
+        elif i == " ":
+            "Baba booey"
+
 def compile():
+    # Variables # 
     ipt = readTxt()
     write = open(py, "w")
-    for l in ipt:
-        if not l.startswith("//"):
-            # logic for the entire esolang will fit into here?
+    tabCount = 0
+    lastTab = 0
+    lineCount = 1
 
+    for l in ipt:
+        # Keep track of how many lines so far # 
+        lineCount = lineCount + 1
+
+        # If it is not a comment then proceed # 
+        if not l.startswith("%") and not l.endswith("%"):
+
+            # Splitting algo makes sure strings are not split :)
+            lines = splitAlgo(l)
+
+            # Tab count stuff
+            lastTab = tabCount
+            for i in l:
+                if i == "{":
+                    tabCount = tabCount + 1
+                if i == "}": 
+                    tabCount = tabCount - 1
+                    lastTab = tabCount
+            l = l.replace("{", "", -1)
+            l = l.replace("}", "", -1)
             
+            # method finder
+            print("Methods Found: ")
             
-            write.write(l)
+            # Dont print anything if it is only curly brackets
+            if l != "{" or l != "}":
+                write.write(((lastTab) * "    ") + l)
     
 
 def run():

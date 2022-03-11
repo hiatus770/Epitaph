b5b69b445e6570408673153340f53199002b7858
hiatus770-chromebook <hiatus.cool@gmail.com>
2022-03-11 11:53:41 -0500

    mogus

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import os
 
 # File Directories #
-txt = "io/input.txt"
+epi = "io/epitaph.epi"
 output = "io/output.txt"
 
 # Returns all lines from the file #
 def readTxt():
-    with open(txt, "r") as ipt:
+    with open(epi, "r") as ipt:
         lines = ipt.readlines() 
         return lines
 
-# Prints out the output of said program # 
+# Prints out the output of compiled program # 
 def printFile(dir = output): 
     with open(dir, "r") as ipt:
         lines = ipt.readlines() 
@@ -20,12 +20,31 @@ def printFile(dir = output):
 
 # Will split line into methods #
 def splitAlgo(l):
-    methods = {}
+    methods = list()
     cnt = 0
-    for g in range(len(l)):
-        cnt = g
-        char = l[cnt]
-        
+    print("LENGTH OF L: 0 -", len(l)-1)
+    for i in range(len(l)):
+        print("In loop: ", i, "\nchar: ", l[i])
+        if l[i] == " " or i == 0:
+            print(i, " is space")
+            cnt = i+1
+            while l[cnt] != " ":
+                cnt = cnt + 1
+                if (cnt == len(l)-1):
+                    print("End of line")
+                    cnt = len(l)
+                    break
+            
+                print(i, cnt, l[cnt])
+
+            if (l[i:cnt] != " ") and i == 0:
+                methods.append(l[i:cnt])
+            elif (l[i:cnt] != " "):
+                methods.append(l[i+1:cnt])
+            
+                
+
+    print(methods)
 
 # Executes a line of code # 
 def executeCode(code):
@@ -51,9 +70,12 @@ def run():
     for l in ipt: 
         # Line count for errors #
         lineCount = lineCount + 1
+        splitAlgo(l)
 
-        # check if its a loop
+
+
+
+
 
 # Compiling to a file, Printing the file, and running it to output.txt
-printFile()
 run()

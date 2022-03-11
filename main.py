8dcb049a2274d0f99f954fb08ccb21583f82cd77
hiatus770 <matiasrivas3106@gmail.com>
2022-03-10 20:58:58 -0500

    mongoose

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import os
 
-# Directories #
+# File Directories #
 txt = "io/input.txt"
 output = "io/output.txt"
-py = "compiled.py"
 
-# Functions #
+# Returns all lines from the file #
 def readTxt():
     with open(txt, "r") as ipt:
         lines = ipt.readlines() 
         return lines
 
-def printFile(dir = py): 
+# Prints out the output of said program # 
+def printFile(dir = output): 
     with open(dir, "r") as ipt:
         lines = ipt.readlines() 
         for line in lines:
             print(line, end = "")
     print()
 
+# Will split line into methods #
 def splitAlgo(l):
     methods = {}
 
+# Executes a line of code # 
+def executeCode(code):
+    "Literally just run one line, how hard could it be :skull:"
 
+# Loop #
+def loop(idk):
+    "I really dont know :("
+
+# If statement # 
+def ifS(idk):
+    "I also dont know"
+
+# Runs entire code # 
 def run():
     # Variables # 
     ipt = readTxt()
-    write = open(py, "w")
-    tabCount = 0
-    lastTab = 0
     lineCount = 1
 
-    for l in ipt:
-        # Keep track of how many lines so far # 
+    var = {} # array keeping track of id to value
+    names = {} # dict keeping track of name to id
+
+    for l in ipt: 
+        # Line count for errors #
         lineCount = lineCount + 1
+
+        # check if its a loop
 
 # Compiling to a file, Printing the file, and running it to output.txt
 printFile()
